ff38f5470125fc4e19c82e8f0eb0235fd4807385
Jonathan <jon.p.bailey@hotmail.co.uk>
2018-12-08 19:03:29 +0000

    Modified to find the total of a sequency of positive/negative integers

--- day1.py
+++ day1.py
@@ -1,1 +1,9 @@
-day 1
+filepath = 'input/day1/day1-puzzle1.txt'
+total = 0
+
+with open(filepath) as fp:
+	line = fp.readline()
+	while line:
+		total += int(line)
+		line = fp.readline()
+print("Frequency {}".format(total))
