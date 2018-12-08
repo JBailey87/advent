e3bea2b5334546a34b8bae3ebb669b03fcc5ffae
Jonathan <jon.p.bailey@hotmail.co.uk>
2018-12-08 20:37:02 +0000

    modified to include initial test for duplicate frequency when cylcing the list of values given. Excpetion thrown when a duplicate is found.

--- day1.py
+++ day1.py
@@ -1,9 +1,33 @@
+import collections
+#filepath = 'input/day1/day1-test.txt'
 filepath = 'input/day1/day1-puzzle1.txt'
-total = 0
+total=0
+list = []
+
+
+
+
+
+def duplicate(frequency):
+	if frequency in list:
+		raise ValueError("duplicate {}".format(frequency))
+
+
 
 with open(filepath) as fp:
 	line = fp.readline()
 	while line:
 		total += int(line)
 		line = fp.readline()
-print("Frequency {}".format(total))
+	print("Total frequency {}".format(total))
+
+total = 0
+
+while True:
+	with open(filepath) as fp:
+		line = fp.readline()
+		while line:
+			total += int(line)
+			duplicate(total)
+			list.append(total)
+			line = fp.readline()
